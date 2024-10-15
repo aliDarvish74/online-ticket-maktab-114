eafae6c55a871d48f7752fc9d46fc5687e8dd447
Mehdi Jannatipour <mahdi-jannati@live.co.uk>
2024-10-15 12:55:08 +0330

    delete line break in comment

--- OnlineTicketAndReservationSystem/App.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlineTicketAndReservationSystem/App.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -85,6 +85,7 @@ namespace App.Web.Areas.Identity.Pages.Account
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
+            ///     
             /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
